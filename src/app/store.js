d609d600f2bb13e1b86667a2b7e7bff0a260c412
TILIKU <85792875+TILIKU@users.noreply.github.com>
2023-03-22 22:34:41 +0400

    added second api to the store(with pain)

--- src/app/store.js
+++ src/app/store.js
@@ -1,11 +1,17 @@
 import { configureStore } from "@reduxjs/toolkit";
 
 import { cryptoApi } from "../services/cryptoApi";
+import { cryptoNewsApi } from "../services/cryptoNewsApi";
+
+const apiArr = [cryptoApi, cryptoNewsApi]
+//apiArr.map((api) => getDefaultMiddleware().concat(api.middleware)),
+
 
 export default  configureStore({
     reducer: {
-        [cryptoApi.reducerPath]: cryptoApi.reducer
+        [cryptoApi.reducerPath]: cryptoApi.reducer,
+        [cryptoNewsApi.reducerPath]: cryptoNewsApi.reducer
     },
     middleware: (getDefaultMiddleware) =>
-        getDefaultMiddleware().concat(cryptoApi.middleware),
+       getDefaultMiddleware().concat(cryptoApi.middleware).concat(cryptoNewsApi.middleware),
 })
